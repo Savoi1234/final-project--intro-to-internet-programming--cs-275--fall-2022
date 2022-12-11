a1ced0cda2b0772af4c40f818cc070b5106973bf
Savoi1234 <112000562+Savoi1234@users.noreply.github.com>
2022-12-11 18:50:39 -0500

    calling html

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    let pageBody = document.querySelector(`body`);
+    let menuButton = document.getElementById(`menu`);
+    let navigationMenu = document.querySelector(`nav`);
+    let modalButton = document.getElementById(`modal`);
+    let modalContent = document.querySelector(`div`);
+
+
+};
